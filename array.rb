444503c9c2b1e8d3b51df534fab19c12ee9ccff8
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-06-08 08:57:27 -0600

    Added calls

--- array.rb
+++ array.rb
@@ -2,3 +2,16 @@ plants = ["trees", "shrubs", "flowers", "ground cover"]
 indexes = [0, 1, 2, 3]
 decimals = [1.1, 2.02, 3.003, 4.004]
 booleans = [true, false, true, false]
+
+#push will add the new element to the array
+plants.push["weeds"]
+
+#pop will remove the last element in the array
+indexes.pop
+
+#shift will remove the first element in the array and return nil
+decimals.shift
+
+#unshift will return the shifted element to the array
+
+booleans.unshift()
